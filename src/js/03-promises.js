57fd4a2fcb8fbd237044e0a0c0994a52d91a1e96
AndriiBilyiV <andrey.dobraf@gmail.com>
2023-08-30 22:36:54 +0300

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    setTimeout(() => {
+      return Promise.resolve({position, delay}).then(({position, delay}) => Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+  },delay)
   } else {
-    // Reject
+    setTimeout(() => {
+      return Promise.reject({position, delay}).catch(({position, delay}) => Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
+    },delay)
+  }  
+}
+
+
+function start(currentDelay, delayStep, amount) {
+  console.log(currentDelay, delayStep, amount, typeof(amount));
+  for (let i = 1; i <= amount; i += 1) {
+    console.log(i)
+      console.log('time')
+      createPromise(i, currentDelay);
+      currentDelay += delayStep;
   }
 }
+
+
+const btn = document.querySelector('button[type="submit"]');
+
+btn.addEventListener('click', (ent) => {
+  ent.preventDefault();
+  const amountValue = Number(document.querySelector('input[name="amount"]').value);
+  const stepValue = Number(document.querySelector('input[name="step"]').value);
+  const delayValue = Number(document.querySelector('input[name="delay"]').value);
+  start(delayValue, stepValue, amountValue)
+})
+
