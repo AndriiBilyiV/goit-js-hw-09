8c3a04baf1c3065d47afe06c96b2f6137b8f8410
AndriiBilyiV <128992399+AndriiBilyiV@users.noreply.github.com>
2023-08-31 17:49:58 +0000

    Deploying to gh-pages from @ AndriiBilyiV/goit-js-hw-09@05cc7094eb8bd91b9c80a04bd5e206e69b3b9572 🚀

---
+++ 01-color-switcher.0ba1ee59.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;function n(){return document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{r=setInterval(n,1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(r),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.0ba1ee59.js.map
---
+++ 01-color-switcher.f326f2a3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function o(){return document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){n=setInterval(o,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.f326f2a3.js.map
